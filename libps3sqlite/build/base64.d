5df318dbdf140ca9ad7b5ff51c00bea707430141
unknown <nippurct@gmail.com>
2020-03-18 16:23:35 +0100

    remove multiple files

--- libps3sqlite/build/base64.d
+++
@@ -1,5 +0,0 @@
-base64.o: \
- C:/PSDK3v2/MinGW/msys/1.0/home/crystal/tmpwork/Ps3_Sqlite/libps3sqlite/source/base64.c \
- C:/PSDK3v2/MinGW/msys/1.0/home/crystal/tmpwork/Ps3_Sqlite/libps3sqlite/source/libavutil/base64.h
-
-C:/PSDK3v2/MinGW/msys/1.0/home/crystal/tmpwork/Ps3_Sqlite/libps3sqlite/source/libavutil/base64.h:
--- libps3sqlite/build/sqlite3.d
+++
@@ -1,2 +0,0 @@
-sqlite3.o: \
- C:/PSDK3v2/MinGW/msys/1.0/home/crystal/tmpwork/Ps3_Sqlite/libps3sqlite/source/sqlite3.c
